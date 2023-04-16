e1c3d8bc1995d5229681fdbc71aed565db1ba412
kokeb19 <kokeby19@gmail.com>
2023-04-16 14:27:09 +0300

    step 10

--- lib/services/auth/auth_provider.dart
+++ lib/services/auth/auth_provider.dart
@@ -2,6 +2,7 @@ import 'auth_user.dart';
 import 'package:mynotes/services/auth/auth_user.dart';
 
 abstract class AuthProvider {
+  Future<void> initialize();
   AuthUser? get currentUser;
   Future<AuthUser> logIn({
     required String email,
--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -5,8 +5,13 @@ import 'package:flutter/foundation.dart';
 @immutable
 class AuthUser {
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified);
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  const AuthUser({required this.isEmailVerified});
+  factory AuthUser.fromFirebase(User user) =>
+      AuthUser(isEmailVerified: user.emailVerified);
+
+  // void testing() {
+  //   AuthUser(isEmailVerified: true);
+  // }
 }
 
 // class MyAuthUser extends AuthUser {
